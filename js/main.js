b00afdd29fce9325ea9a68745bf382586fee94cb
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-08-19 17:54:33 +0100

    basic app tracks number of fingers and hands visible

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
-$(document).ready(docReady);
+var lm = lm || {};
 
-function docReady() {
-	//coad of coad hall
+lm.debugMode = true; //set to true to see console logs of variables
+lm.i = 0; //handy counter for debugging
+
+lm.prevHands = 0;
+lm.prevFingers = 0;
+
+lm.hands = 0;
+lm.fingers = 0;
+
+lm.changed = function() {
+	return (lm.hands != lm.prevHands) || (lm.fingers != lm.prevFingers) ;
 }
+
+lm.updateView = function() {
+	$('#numHands').html(lm.hands);
+	$('#numFingers').html(lm.fingers);
+}
+
+lm.main = function() {
+	Leap.loop(function(obj) {
+
+		//store previous hand/finger count
+		lm.prevHands = lm.hands;
+		lm.prevFingers = lm.fingers;
+
+		//count hands
+		lm.hands = obj.hands.length;
+
+		//count fingers if there's at least one hand
+		if(lm.hands > 0) {
+			lm.fingers = obj.fingers.length;
+		}
+
+
+		if(lm.changed() && lm.debugMode)
+		{
+			console.log(obj);
+			console.log("%clm.hands: " + lm.hands, "background-color:black;color:white;font-size:150%");
+			console.log("%clm.fingers: " + lm.fingers, "background-color:black;color:white;font-size:150%");
+		};
+
+		if(lm.changed())
+			lm.updateView(); //IT'S FUNCTION NAMES LIKE THIS THAT MAKE ME WISH I'D WRITTEN THIS APP WITH ANGULAR
+	})
+};
+
+$(document).ready(lm.main);
